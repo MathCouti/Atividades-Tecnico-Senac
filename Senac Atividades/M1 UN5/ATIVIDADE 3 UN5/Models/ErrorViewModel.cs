2bc9b333589a8c0ced258b5d160fb86e45870fa2
MathCouti <mathcoutipro@gmail.com>
2022-08-09 18:01:32 -0300

    criação atividade 3

---
+++ Senac Atividades/M1 UN5/ATIVIDADE 3 UN5/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ATIVIDADE_3_UN5.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
